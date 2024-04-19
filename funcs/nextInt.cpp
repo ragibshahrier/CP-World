70353bc41413077e595e2af07265aee847e9a78e
ragibshahrier <69706704+ragibshahrier@users.noreply.github.com>
2024-04-20 03:29:06 +0600

    Create nextInt.cpp
    function to take inputs seperated with space comma or anything else

---
+++ funcs/nextInt.cpp
@@ -0,0 +1,16 @@
+int nextInt() {
+    int x = 0, p = 1;
+    char c;
+    do {
+        c = getchar();
+    } while (c <= 32);
+    if (c == '-') {
+        p = -1;
+        c = getchar();
+    }
+    while (c >= '0' && c <= '9') {
+        x = x * 10 + c - '0';
+        c = getchar();
+    }
+    return x * p;
+}
